1d51e66b8f88f66ffe6a666e0b799a90f9460261
Anton Shcherbei <starteran@gmail.com>
2023-07-16 16:29:42 +0300

    fix issues

--- cypress/e2e/login.cpec.cy.js
+++ cypress/e2e/login.cpec.cy.js
@@ -4,21 +4,18 @@ describe('login site ',() => {
         cy.title().should('include','ParaBank');
     })
 
-
     it('login fields are displayed',() => {
-        cy.get('input[name="username"]').should('be visible')
-        cy.get('input[name="password"]').should('be visible')
+        cy.get('input[name="username"]').should('be.visible')
+        cy.get('input[name="password"]').should('be.visible')
     })
 
     it('login  with unvalid credentials',() => {
+        cy.visit('https://parabank.parasoft.com/')
         cy.get('input[name="username"]').type('///')
         cy.get('input[name="password"]').type('///')
+        cy.get('[type="submit"]').click()
         cy.get('[class="error"')
-        .should('be visible')
-        .should('have text','The username and password could not be verified.')
-    })
-
-    after(() => {
-        cy.close()
+        .should('be.visible')
+        .should('have.text','The username and password could not be verified.')
     })
 })
